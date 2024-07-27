63d69b0286be087b23d51d9d3c6d311c78921d13
wogus <antrocket825@gmail.com>
2024-07-27 20:18:37 +0700

    add button for moving to generate room page

--- app/src/main/java/com/example/taxi/ui/MainActivity.kt
+++ app/src/main/java/com/example/taxi/ui/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.taxi.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taxi.R
+import com.example.taxi.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.createRoom.setOnClickListener {
+            startActivity(Intent(this, GenerateRoomActivity::class.java))
+        }
     }
+
 }
